6417502a7e4dd7ffde37445893781a43e3243c1f
ThiloAmmon <thiloammon@gmail.com>
2020-03-07 21:49:42 +0100

    Test for equality of numpy arrays

--- TestCoordinateSystem.py
+++ TestCoordinateSystem.py
@@ -4,13 +4,16 @@ import unittest
 class TestCoordinateSystem(unittest.TestCase):
     def test_coordinateSystemForOnePart(self):
         import numpy as np
+        import numpy.testing as nptesting
         from coordinateSystem import getCoordinateSystem
 
         cube_part = [[[1]]]
-        self.assertEqual(getCoordinateSystem([cube_part], [1, 1, 1]), np.array([[[0.]]]))
+        nptesting.assert_equal(getCoordinateSystem([cube_part], [1, 1, 1]), np.array([[[0.]]]))
 
         cube_part = [[[1, 1]]]
-        self.assertEqual(getCoordinateSystem([cube_part], [1, 1, 1]), np.zeros((3, 3, 3)))
+        nptesting.assert_equal(getCoordinateSystem([cube_part], [1, 1, 1]), np.zeros((3, 3, 3)))
 
 if __name__ == '__main__':
     unittest.main()
+
+
