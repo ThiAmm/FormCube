763c80bf9290019325bbc8a88fdf7bc07ce7120f
ThiloAmmon <thiloammon@gmail.com>
2020-03-08 17:30:01 +0100

    Tests for calculating the reference point of the object

--- TestCoordinateSystem.py
+++ TestCoordinateSystem.py
@@ -1,10 +1,12 @@
 import unittest
 
+from coordinateSystem import getPointToReferenceCornerOfCube
+import numpy as np
+import numpy.testing as nptesting
+
 
 class TestCoordinateSystem(unittest.TestCase):
     def test_coordinateSystemForOnePart(self):
-        import numpy as np
-        import numpy.testing as nptesting
         from coordinateSystem import getCoordinateSystem
 
         cube_part = [[[1]]]
@@ -13,7 +15,12 @@ class TestCoordinateSystem(unittest.TestCase):
         cube_part = [[[1, 1]]]
         nptesting.assert_equal(getCoordinateSystem([cube_part], [1, 1, 1]), np.zeros((3, 3, 3)))
 
+    def test_ReferenceCornerOfDesiredObject(self):
+        cube_part = [[1]]
+        self.assertEqual(getPointToReferenceCornerOfCube([cube_part], (1, 1, 1), 3), [0, 0, 0])
+        cube_part = [[1, 1]]
+        self.assertEqual(getPointToReferenceCornerOfCube([cube_part], (1, 1, 1), 3), [-1, -1, -1])
+
+
 if __name__ == '__main__':
     unittest.main()
-
-
--- coordinateSystem.py
+++ coordinateSystem.py
@@ -6,9 +6,13 @@ def getCoordinateSystem(cube_parts, shape, dimension=3):
     return np.zeros([length_of_coordinateSystem_in_all_dimensions] * dimension)
 
 
-def getPointToReferenceCornerOfCube(cube_parts, dimension):
+def getPointToReferenceCornerOfCube(cube_parts, shape, dimension):
+    from operator import add
+
+    coordinateSystem = getCoordinateSystem(cube_parts, shape, dimension)
     maximal_length_of_cube_part = getMaximalLengthOfCubePart(cube_parts)
-    return [maximal_length_of_cube_part] * dimension
+    coordinateSystem_Edge = tuple(-1*x for x in coordinateSystem.shape)
+    return list(map(add, coordinateSystem_Edge, [maximal_length_of_cube_part] * dimension))
 
 
 def getMaximalLengthOfCubePart(cube_parts):
