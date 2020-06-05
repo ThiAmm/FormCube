0338a3c3c1354816d6c0ce1b5ef9d6515366f751
ThiloAmmon <thiloammon@gmail.com>
2020-06-05 22:41:20 +0200

    Test for the number of combinations of two cubes

--- TestCombinations.py
+++ TestCombinations.py
@@ -18,5 +18,15 @@ class TestCoordinateSystem(unittest.TestCase):
                                         dimension=3)
         nptesting.assert_equal(len(combinations), 24)
 
+    def test_combinationForTwoSimpleObjects(self):
+        cube_parts = [np.array([[[1]]]), np.array([[[1]]])]
+        coordinate_system = np.array([[[0.], [0.]]])
+        combinations = get_combinations(coordinate_system,
+                                        point_to_reference_corner_of_cube=[0, 0, 0],
+                                        cube_parts=cube_parts,
+                                        shape=(1, 2, 1),
+                                        dimension=3)
+        nptesting.assert_equal(len(combinations), 24 * 24 * 4)
+
     if __name__ == '__main__':
         unittest.main()
